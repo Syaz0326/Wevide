282e36e694c602f1676274e3edc37663e42531d7
Syaz0326 <tadakazu326@gmail.com>
2022-05-12 08:21:25 +0900

    chore: Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  root: "./src",
+  build: {
+    outDir: path.join(__dirname, "build"),
+  },
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src"),
+    },
+  },
+  plugins: [react()],
+});
