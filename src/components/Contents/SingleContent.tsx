f77db96e8895c43f81b3828bc89668546b46fe05
Syaz0326 <tadakazu326@gmail.com>
2022-05-22 22:52:03 +0900

    refactor: navigation bar height to constant

--- src/components/Contents/SingleContent.tsx
+++ src/components/Contents/SingleContent.tsx
@@ -2,6 +2,8 @@ import React, { useRef } from 'react';
 import { Grid, SxProps } from '@mui/material';
 import { NavigationBar } from '../Elements';
 
+const NAVIGATION_HEIGHT = '36px';
+
 export type SingleContentProps = {
   id: string;
   url: URL;
@@ -13,11 +15,11 @@ export const SingleContent = ({ id, url, sx }: SingleContentProps) => {
 
   return (
     <Grid container sx={{ height: '100vh', width: '100%', ...sx }}>
-      <NavigationBar wv={wv} homeUrl={url} sx={{ height: '36px' }} />
+      <NavigationBar wv={wv} homeUrl={url} sx={{ height: NAVIGATION_HEIGHT }} />
       <webview
         ref={wv}
         src={url.href}
-        style={{ height: 'calc(100vh - 36px)', width: '100%' }}
+        style={{ height: `calc(100vh - ${NAVIGATION_HEIGHT})`, width: '100%' }}
         allowpopups
         webpreferences="nativeWindowOpen=yes,nodeIntegration=no,spellcheck-yes,contextIsolation=no,any=no,webSecurity=no"
         partition={partition}
